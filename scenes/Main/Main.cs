dfecae99f22451514c82a05ef31ebd7758bd97c0
McCune1224 <alexmccune1224@gmail.com>
2025-01-12 21:26:48 -0500

    setup graph library with bag class

--- scenes/Main/Main.cs
+++ scenes/Main/Main.cs
@@ -1,13 +1,21 @@
 using Godot;
 using System;
+using Graph;
 
 public partial class Main : Node2D
 {
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        GD.Print("I EXIST IN MAIN!");
-
+        GD.Print("What's good brah");
+        Graph.Bag<int> bag = new();
+        bag.Add(1);
+        bag.Add(4);
+        bag.Add(9);
+        foreach (int item in bag)
+        {
+            GD.Print(item);
+        }
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
